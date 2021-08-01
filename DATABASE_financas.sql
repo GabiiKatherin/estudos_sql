a08402b03cc5a6ec23cdcfb3c105cc2232efa975
Gabii Katherin <katheringabrielli@gmail.com>
2021-08-01 14:15:48 -0300

    Update DATABASE_financas.sql
    Inclusao da TABLE conta_corrente

--- DATABASE_financas.sql
+++ DATABASE_financas.sql
@@ -36,3 +36,22 @@ CREATE TABLE IF NOT EXISTS cliente(
 	PRIMARY KEY(numero)
 );
 
+CREATE TABLE conta_corrente(
+	/*Fará referência a vários bancos*/
+	banco_numero INTEGER NOT NULL,
+	agencia_numero INTEGER NOT NULL,
+	numero BIGINT NOT NULL,
+	digito SMALLINT NOT NULL,
+	/*BIGINT proque não é um num serial*/
+	cliente_numero BIGINT NOT NULL,
+	
+	/*Comandos de controle*/	
+	ativo BOOLEAN NOT NULL DEFAULT TRUE,
+	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	/*Poderíamos aqui criar um id pra agrupar tudo isso, mas em matéria de regras de normalização é importante que a indentidade da tabela esteja descrita naquela PRIMARY KEY, por isso, aqui vamos colocar dessa forma*/
+	PRIMARY KEY (banco_numero,agencia_numero,numero,digito,cliente_numero),
+	
+	/*Uma FOREIGN KEY tem vai referenciar uma PRIMARY KEY com mais de um campo, precisa ter os mesmos campos*/
+	FOREIGN KEY (banco_numero,agencia_numero) REFERENCES agencia (numero,banco_numero),
+	FOREIGN KEY (cliente_numero) REFERENCES cliente (numero)
+);
