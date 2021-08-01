0329b510ab9b389d46a0c7dc288181fef7260282
Gabii Katherin <katheringabrielli@gmail.com>
2021-08-01 20:57:14 -0300

    Update DDL (DATABASE_financas).sql
    Alterações no banco para correção de erro ao inserir dados:
    ERROR:  insert or update on table "conta_corrente" violates foreign key constraint "conta_corrente_banco_numero_fkey"
    DETAIL:  Key (banco_numero, agencia_numero)=(1, 2) is not present in table "agencia".
    SQL state: 23503

--- DDL (DATABASE_financas).sql
+++ DDL (DATABASE_financas).sql
@@ -1,12 +1,10 @@
 CREATE TABLE IF NOT EXISTS banco (
 	numero INTEGER NOT NULL,
 	nome VARCHAR(50) NOT NULL,
-	
-	/* Boas práticas */
-	
 	ativo BOOLEAN NOT NULL DEFAULT TRUE,
+
+	/*Boas práticas*/
 	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	
 	PRIMARY KEY (numero)
 );
 
@@ -22,17 +20,16 @@ CREATE TABLE IF NOT EXISTS agencia (
 	FOREIGN KEY (banco_numero) REFERENCES banco (numero)
 );
 
-
-/*Tudo que for imediato de olhar, separe numa tabela
-Para que as tabelas não concorram entre si*/
+/*Tudo que for imediato de olhar, separe numa tabela diferente para que as tabelas não concorram entre si*/
 CREATE TABLE IF NOT EXISTS cliente(
-	numero BIGSERIAL PRIMARY KEY,
+	numero BIGSERIAL NOT NULL,
 	nome VARCHAR(120) NOT NULL,
 	email VARCHAR(250) NOT NULL,
 	
 	ativo BOOLEAN NOT NULL DEFAULT TRUE,
 	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	
+	PRIMARY KEY(numero)
 );
 
 CREATE TABLE IF NOT EXISTS conta_corrente(
@@ -51,7 +48,7 @@ CREATE TABLE IF NOT EXISTS conta_corrente(
 	PRIMARY KEY (banco_numero,agencia_numero,numero,digito,cliente_numero),
 	
 	/*Uma FOREIGN KEY tem vai referenciar uma PRIMARY KEY com mais de um campo, precisa ter os mesmos campos*/
-	FOREIGN KEY (banco_numero,agencia_numero) REFERENCES agencia (numero,banco_numero),
+	FOREIGN KEY (banco_numero,agencia_numero) REFERENCES agencia (banco_numero,numero),
 	FOREIGN KEY (cliente_numero) REFERENCES cliente (numero)
 );
 
@@ -61,27 +58,27 @@ CREATE TABLE IF NOT EXISTS tipo_transacao(
 
 	ativo BOOLEAN NOT NULL DEFAULT TRUE,
 	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-
 );
 
-CREATE TABLE IF NOT EXISTS cliente_transacoes(
+CREATE TABLE IF NOT EXISTS cliente_transacoes (
 	/*Se essa TABLE vai referenciar uma transação do cliente, ela envolve a conta corrente dele o que implica a inclusao de uma referencia*/
 	id BIGSERIAL PRIMARY KEY,
-	
 	banco_numero INTEGER NOT NULL,
 	agencia_numero INTEGER NOT NULL,
 	conta_corrente_numero BIGINT NOT NULL,
 	conta_corrente_digito SMALLINT NOT NULL,
 	cliente_numero BIGINT NOT NULL,
 	tipo_transacao_id SMALLINT NOT NULL,
-	
 	/*O valor pode ter 15 numeros e os dois ultimos são casas decimais*/
-	valor NUMERIC(15,2),
-
-	FOREIGN KEY (banco_numero,agencia_numero,conta_corrente_numero,conta_corrente_digito,cliente_numero) REFERENCES conta_corrente(banco_numero,agencia_numero,numero,digito,cliente_numero),
-	
+	valor NUMERIC(15,2) NOT NULL,
 	/*Não vamos inserir o campo "ativo" porque é uma transação e ela irá ocorrer*/
-	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (banco_numero,agencia_numero,conta_corrente_numero,conta_corrente_digito,cliente_numero) REFERENCES conta_corrente(banco_numero,agencia_numero,numero,digito,cliente_numero)
 );
 
 /*Agora iremos inserir dados respeitando as PRIMARY KEY's e FOREIGN KEY's*/
+
+
+
+
+
